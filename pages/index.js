79c29f026a322790b870fea95038a38970bc0bc2
Roman Romanyshyn <romanromanyshyn@Roman-MacBook-Air.local>
2023-08-15 12:57:59 +0700

    admin panel completed

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import Layout from "@/components/Layout";
 import Products from "./products";
 import { useSession } from "next-auth/react";
+import Image from "next/image";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,7 +16,7 @@ export default function Home() {
           Hello : <b> {session?.user?.name}</b>
         </h1>
         <div className="flex bg-gray-300 gap 1 text-black rounded-lg overflow-hidden">
-          <img src={session?.user?.image} alt="photo" className="w-6 h-6"></img>
+          <Image src={session?.user?.image} alt="photo" className="w-6 h-6" />
           <span className=" px-2"> </span>
           {session?.user?.name}
         </div>
